6c37c6976322933ba4094660f176255bb07b59c6
Julius <medicjue@gmail.com>
2017-03-15 23:52:48 +0800

    Implement pollutantmean.R

---
+++ week2/main.R
@@ -0,0 +1,9 @@
+setwd("/Users/Julius/JHU_Rlang_HW/jhu_Rlang_homework/week2")
+
+source("pollutantmean.R")
+
+pollutantmean("specdata", "sulfate", 1:10)
+
+pollutantmean("specdata", "nitrate", 70:72)
+
+pollutantmean("specdata", "nitrate", 23)
