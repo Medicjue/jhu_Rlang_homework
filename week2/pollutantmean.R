25d8dee0b8087b7dec797c4a1c9c4cee57eda4ac
Julius <medicjue@gmail.com>
2017-03-16 07:24:46 +0800

    Only declaration for correlation function homework

--- week2/pollutantmean.R
+++ week2/pollutantmean.R
@@ -2,9 +2,9 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   allFiles <- as.character( list.files(directory) )
   filePaths <- paste(directory, allFiles, sep="/")
   allData <- {}
-	for(fileId in id) {
-	  singleData <- read.csv(filePaths[fileId], header = T)
-	  allData <- rbind(allData, singleData)
-	}
-	return(mean(allData[,pollutant], na.rm = T))
+  for(fileId in id) {
+    singleData <- read.csv(filePaths[fileId], header = T)
+    allData <- rbind(allData, singleData)
+  }
+  return(mean(allData[,pollutant], na.rm = T))
 }
